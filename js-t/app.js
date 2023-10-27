c8763cfde976d14870835c4bda5efc794fcb9b23
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-27 20:45:17 +0000

    abc

--- js-t/app.js
+++ js-t/app.js
@@ -1,10 +1,43 @@
 function Hack() {
     var textToChange = document.getElementById("Text").value;
-    document.getElementById("hackme").innerHTML = textToChange
+    document.getElementById("hackme").innerHTML = textToChange;
     document.getElementById("Text").value = "";
-}
+};
 
-function change_back() {
-    let text = prompt("em desenvolvimento")
-    alert(text)
-}
+function change_backD() {
+    let input_text = document.getElementById("Text");
+    let buttons = document.getElementById("button");
+    let footer = document.getElementById("footer");
+    let body = document.getElementById("corpo");
+    let bodyValue = document.getElementById("corpo").className.value;
+
+    if (bodyValue = "light_body") {
+        var themeButton = document.getElementsByName("theme");
+        themeButton.onclick = change_backL();
+    }
+    else {
+        input_text.className = "light_input_text";
+        buttons.className = "light.button";
+        footer.className = "light_embaixo"
+        body.className = "light_body"
+    }
+};
+
+function change_backL() {
+    let input_text = document.getElementById("Text");
+    let buttons = document.getElementById("button");
+    let footer = document.getElementById("footer");
+    let body = document.getElementById("corpo");
+    let bodyValue = document.getElementById("corpo").className.value;
+
+    if (bodyValue = "dark_body") {
+        var themeButton = document.getElementsByName("theme");
+        themeButton.onclick = change_backD();
+    }
+    else {
+        input_text.className = "dark_input_text";
+        buttons.className = "dark.button";
+        footer.className = "dark_embaixo"
+        body.className = "dark_body"
+    }
+};
