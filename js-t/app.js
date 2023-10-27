1f15cec5144aaae4b07c0bf5640e0ac3d0b17550
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-27 21:04:48 +0000

    return

--- js-t/app.js
+++ js-t/app.js
@@ -4,40 +4,40 @@ function Hack() {
     document.getElementById("Text").value = "";
 };
 
-function change_backD() {
-    let input_text = document.getElementById("Text");
-    let buttons = document.getElementById("button");
-    let footer = document.getElementById("footer");
-    let body = document.getElementById("corpo");
-    let bodyValue = document.getElementById("corpo").className.value;
+//function change_backD() {
+  //  let input_text = document.getElementById("Text");
+    //let buttons = document.getElementById("button");
+   // let footer = document.getElementById("footer");
+   // let body = document.getElementById("corpo");
+    //let bodyValue = document.getElementById("corpo").className.value;
 
-    if (bodyValue = "light_body") {
-        var themeButton = document.getElementsByName("theme");
-        themeButton.onclick = change_backL();
-    }
-    else {
-        input_text.className = "light_input_text";
-        buttons.className = "light.button";
-        footer.className = "light_embaixo"
-        body.className = "light_body"
-    }
-};
+//    if (bodyValue = "light_body") {
+//        var themeButton = document.getElementsByName("theme");
+//        themeButton.onclick = change_backL();
+//   }
+ //   else {
+ //       input_text.className = "light_input_text";
+    //    buttons.className = "light.button";
+ //       footer.className = "light_embaixo"
+   //     body.className = "light_body"
+  //  }
+//};
 
-function change_backL() {
-    let input_text = document.getElementById("Text");
-    let buttons = document.getElementById("button");
-    let footer = document.getElementById("footer");
-    let body = document.getElementById("corpo");
-    let bodyValue = document.getElementById("corpo").className.value;
+//function change_backL() {
+ //   let input_text = document.getElementById("Text");
+  //  let buttons = document.getElementById("button");
+  //  let footer = document.getElementById("footer");
+  //  let body = document.getElementById("corpo");
+  //  let bodyValue = document.getElementById("corpo").className.value;
 
-    if (bodyValue = "dark_body") {
-        var themeButton = document.getElementsByName("theme");
-        themeButton.onclick = change_backD();
-    }
-    else {
-        input_text.className = "dark_input_text";
-        buttons.className = "dark.button";
-        footer.className = "dark_embaixo"
-        body.className = "dark_body"
-    }
-};
+  //  if (bodyValue = "dark_body") {
+  //      var themeButton = document.getElementsByName("theme");
+  //      themeButton.onclick = change_backD();
+   // }
+  //  else {
+  //      input_text.className = "dark_input_text";
+  //      buttons.className = "dark.button";
+  ////      footer.className = "dark_embaixo"
+  //      body.className = "dark_body"
+  //  }
+// };
